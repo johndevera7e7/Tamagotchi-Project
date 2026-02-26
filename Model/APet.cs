f0fed53020998f10985e080792b405a767d7d2fb
johndevera7e7 <john.devera.7e7@itb.cat>
2026-02-26 11:38:10 +0100

    feat: Abstract class "Pet" completed.

--- Model/APet.cs
+++ Model/APet.cs
@@ -1,23 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Tamagotchi.Model
 {
-    public class APet
+    public abstract class APet
     {
+        protected const string NAME = "Unknown";
+        protected const int AGE = 0;
+
         protected string Name { get; set; }
         protected int Age { get; set; }
 
-        public virtual string GetName()
-        {
-            return this.Name;
-        }
-
-        public virtual void SetName(string name)
+        public APet (string name, int age)
         {
             this.Name = name;
+            this.Age = age;
+        }
+
+        public APet (int age) 
+        {
+            this.Age = age;
+            this.Name = NAME;
+        }
+
+        public APet(string name)
+        {
+            this.Age = AGE;
+            this.Name = name;
+        }
+
+        public APet()
+        {
+            this.Age = AGE;
+            this.Name = NAME;
         }
     }
 }
