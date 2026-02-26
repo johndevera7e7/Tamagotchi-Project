c5fb8d210662057be2d5557912d2ed93ab708d92
johndevera7e7 <john.devera.7e7@itb.cat>
2026-02-26 10:23:21 +0100

    Initial Commit

---
+++ Model/APet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Tamagotchi.Model
+{
+    public class APet
+    {
+        protected string Name { get; set; }
+        protected int Age { get; set; }
+
+        public virtual string GetName()
+        {
+            return this.Name;
+        }
+
+        public virtual void SetName(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
