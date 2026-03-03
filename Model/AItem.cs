dc3c5ffb189c6009115f9be235824d454ec802f6
johndevera7e7 <john.devera.7e7@itb.cat>
2026-03-03 14:32:59 +0100

    Feat: added class inventory, Player, Item and food, closes #16, closes#29, closes #13, closes #10, closes #2

---
+++ Model/AItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchi.Model
+{
+    public abstract class AItem
+    {
+        protected const int USES = 3;
+        protected int Uses { get; set; }
+
+        public AItem (int uses)
+        {
+            this.Uses = uses;
+        }
+
+        public AItem()
+        {
+            this.Uses = USES;
+        }
+    }
+}
---
+++ Model/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchi.Model
+{
+    public class Player
+    {
+        private const string NAME = "No player name.";
+        private string Name { get; set; }
+
+        public Player (string name)
+        {
+            this.Name = name;
+        }
+
+        public Player()
+        {
+            this.Name = NAME;
+        }
+    }
+}
