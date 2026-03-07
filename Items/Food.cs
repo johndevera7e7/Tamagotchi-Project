c0e9f84ee340dfd02efeaae3189fc3ea54c0d1c0
johndevera7e7 <john.devera.7e7@itb.cat>
2026-03-07 14:04:36 +0100

    feat: added item classes, closes #14

---
+++ Items/Food.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchi.Items
+{
+    public class Food: AItem
+    {
+        protected int Nutrition { get; set;}
+        protected FoodItem FoodItemType { get; set; }
+        public enum FoodItem
+        {
+            Meat,
+            Seeds,
+            Fruit_Bowl
+        }
+
+        public Food(FoodItem index)
+        {
+            switch (index)
+            {
+                case FoodItem.Meat:
+                    this.FoodItemType = FoodItem.Meat;
+                    this.Uses = 3;
+                    this.Name = "Meat";
+                    break;
+                case FoodItem.Seeds:
+                    this.FoodItemType = FoodItem.Seeds;
+                    this.Uses = 5;
+                    this.Name = "Seeds";
+                    break;
+                case FoodItem.Fruit_Bowl:
+                    this.FoodItemType = FoodItem.Fruit_Bowl;
+                    this.Uses = 2;
+                    this.Name = "Fruit bowl";
+                    break;
+            }
+        }
+    }
+}
---
+++ Items/Tool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchi.Items
+{
+    public class Tool: AItem
+    {
+        protected ToolItem ToolItemType { get; set; }
+        public enum ToolItem
+        {
+            Oil,
+            Screwdriver
+        }
+
+        public Tool(ToolItem index)
+        {
+            switch (index)
+            {
+                case ToolItem.Oil:
+                    this.ToolItemType = ToolItem.Oil;
+                    this.Uses = 4;
+                    this.Name = "Oil";
+                    break;
+                case ToolItem.Screwdriver:
+                    this.ToolItemType = ToolItem.Screwdriver;
+                    this.Uses = 2;
+                    this.Name = "Screwdriver";
+                    break;
+                
+            }
+        }
+    }
+}
